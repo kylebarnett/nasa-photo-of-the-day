e8c2b7821405e3f406ea7afaaffb6435283a7adf
Kyle Barnett <kbarnett23@gmail.com>
2019-07-30 19:40:52 -0600

    more styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
   return (
     <div className="App">
       <div className="header-container">
-        <h1>Nasa Photo of the Day!</h1>
+        <nav className="nav-container">
+          <button className="buttons" href="https://nasa.gov">Home</button>
+          <button className="buttons " href="https://github.com/kylebarnett">About Me </button>
+        </nav>
+        <div className="title">
+          <h1>Nasa Photo of the Day!</h1>
+        </div>
       </div>
       <NasaPhotos />
     </div>
