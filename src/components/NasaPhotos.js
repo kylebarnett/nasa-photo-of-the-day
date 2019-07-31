64c41b8beb48baa9032fc7a8b51fe120fc0685a9
Kyle Barnett <kbarnett23@gmail.com>
2019-07-31 12:27:00 -0600

    added some more

--- src/components/NasaPhotos.js
+++ src/components/NasaPhotos.js
@@ -4,22 +4,31 @@ import axios from 'axios'
 
 export default function NasaPhotos(props) {
   const [pictures, setPicture] = useState([])
+  const [copyright, setCopyright] = useState([])
+  const [date, setDate] = useState([])
+  const [explanation, setExplanation] = useState([])
   const [isLoading, setIsLoading] = useState(false)
 
-  useEffect(date => {
+  useEffect(() => {
     setIsLoading(true)
     axios
       .get('https://api.nasa.gov/planetary/apod?api_key=qmhUy9yMuXQcOs8PNEE978acegMnrl1mdKCZxMgn')
       .then(res => {
         console.log(res)
         setPicture(res.data.url)
+        setCopyright(res.data.copyright)
+        setDate(res.data.date)
+        setExplanation(res.data.explanation)
         setIsLoading(false)
       })
   }, [pictures])
   return (
     <div>
       {isLoading && <h1>Your Nasa Picture is Loading...</h1>}
-      <Photo img={pictures}/>
+      <h1>Photographer: {copyright}</h1>
+      <h2>Date: {date}</h2>
+      <p>{explanation}</p>
+      <Photo img={pictures} />
     </div>
   )
 }
